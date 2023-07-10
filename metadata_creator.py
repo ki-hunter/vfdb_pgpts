2f44a33455b91fd178778bc35f37dddaccb0ad34
ki-hunter <93654571+ki-hunter@users.noreply.github.com>
2023-07-10 15:54:18 +0200

    initial bacdive client
    need to add filtering for pathogen data, matching with downloaded species and actual metadata csv file creation

--- metadata_creator.py
+++ metadata_creator.py
@@ -1,26 +1,55 @@
 import urllib.request
 import os
 import time
+import bacdive
+import pathlib
 
 
-
-def ncbi_datasets_installer():
-    dir_content = os.listdir()
+# def ncbi_datasets_installer():
+#     dir_content = os.listdir()
     
-    if not "datasets" in dir_content:
-        print("NCBI command line tools 'datasets' not found, installing...")
-        os.system("curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets'")
-        os.system("chmod +x datasets")
-    else:
-        os.system("chmod +x datasets")
+#     if not "datasets" in dir_content:
+#         print("NCBI command line tools 'datasets' not found, installing...")
+#         os.system("curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets'")
+#         os.system("chmod +x datasets")
+#     else:
+#         os.system("chmod +x datasets")
     
-    return True
+#     return True
+
+def credential_finder(credential_dir:pathlib.Path):
+
+    with open(credential_dir, "r") as file:
+        credentials = file.readlines()
+    credentials[0] = credentials[0][:-1]
+
+    credentials = {"em" : credentials[0], "pw" : credentials[1]}
+
+    return credentials
 
 
 
-def species_name_finder(genus_dir:str):
-    
-    
+def bacdive_client(genus_name:str):
+
+    bacdive_credentials = pathlib.Path("C:/Users/Kilian/Documents/bacdive.txt")
+
+    credentials = credential_finder(bacdive_credentials)
+
+    em = credentials["em"]
+    pw = credentials["pw"]
+
+    client = bacdive.BacdiveClient(em, pw)
+
+    genus_count =client.search(taxonomy=genus_name)
+
+    for strain in client.retrieve():
+        #print(strain)
+        pass
+
+    filter = ["keywords", "culture collection no."]
+
+    result = client.retrieve(filter)
+    print({k:v for x in result for k,v in x.items()})
     
     return True
 
@@ -29,6 +58,8 @@ def species_name_finder(genus_dir:str):
 def main():
     # ncbi_datasets_installer()
     #species_name_finder()
+    bacdive_client("Pseudomonas")
+
     pass
 
 
